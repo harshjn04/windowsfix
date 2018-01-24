a26bc7f1bd0ba29b16d07e72abcb94084518b7c5
harshjn04 <34502394+harshjn04@users.noreply.github.com>
2018-01-24 11:43:27 +0530

    Update windows_fix.cmd

--- windows_fix.cmd
+++ windows_fix.cmd
@@ -3,3 +3,4 @@ echo 127.0.0.1 store.corp.adobe.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v TabProcGrowth /t REG_DWORD /d 0
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BFCACHE" /v iexplore.exe /t REG_DWORD /d 0
