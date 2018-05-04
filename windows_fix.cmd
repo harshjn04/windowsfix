af845eaffa21738069fb52818c9f9eff45938b3a
harshjn04 <34502394+harshjn04@users.noreply.github.com>
2018-05-04 14:17:19 +0530

    Update windows_fix.cmd

--- windows_fix.cmd
+++ windows_fix.cmd
@@ -1,2 +1,2 @@
 @echo off
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v TabProcGrowth /t REG_DWORD /d 0
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v TabProcGrowth /t REG_DWORD /d 0 /f
