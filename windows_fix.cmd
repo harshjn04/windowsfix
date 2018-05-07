93c95de022042842f441744f24767f6ccc3f82d5
harshjn04 <34502394+harshjn04@users.noreply.github.com>
2018-05-07 16:00:19 +0530

    Update windows_fix.cmd

--- windows_fix.cmd
+++ windows_fix.cmd
@@ -1,3 +1,4 @@
 @echo off
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v TabProcGrowth /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BFCACHE" /v iexplore.exe /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main" /v TabProcGrowth /t REG_DWORD /d 0 /f
